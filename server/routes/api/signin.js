cf7b6f1b41c3dc2c17c168a6ce6285fadf04c764
Edwin Mugambi <edwinnjeru77@gmail.com>
2023-05-25 12:23:21 +0300

    :artist: removed callback function

--- server/routes/api/signin.js
+++ server/routes/api/signin.js
@@ -27,54 +27,56 @@ router.post("/", (req, res) => {
   } //field are not empty
 
   //make sure that the admin exists
-  Admin.find({ userName: userName }, (err, admins) => {
-    if (err) {
-      return res.send({
-        success: false,
-        message: "Server Error",
-      });
-    }
-    if (admins == 0) {
-      return res.send({
-        success: false,
-        message: "The Admin does not exist",
-      });
-    }
-
-    //user does exist
-    //check if password is correct for that particular admin
-    const admin = admins[0];
-    if (!admin.validPassword(password)) {
-      return res.send({
-        success: false,
-        message: "The password is incorrect",
-      });
-    }
-
-    //Everything is OK
-    //create an Admin session
-
-    const newAdminSession = new AdminSession({
-      adminId: admin.id,
-    });
-
-    //save the new user session
-
-    newAdminSession.save((err, session) => {
+  Admin.find({ userName: userName }).then(
+    (err, admins) => {
       if (err) {
         return res.send({
           success: false,
-          message: "Server Error",
+          message: err,
         });
       }
-
-      res.send({
-        success: true,
-        message: "User Successfully Signed In",
-        token: session.id,
+      if (admins == 0) {
+        return res.send({
+          success: false,
+          message: "The Admin does not exist",
+        });
+      }
+  
+      //user does exist
+      //check if password is correct for that particular admin
+      const admin = admins[0];
+      if (!admin.validPassword(password)) {
+        return res.send({
+          success: false,
+          message: "The password is incorrect",
+        });
+      }
+  
+      //Everything is OK
+      //create an Admin session
+  
+      const newAdminSession = new AdminSession({
+        adminId: admin.id,
       });
-    });
-  });
+  
+      //save the new user session
+  
+      newAdminSession.save((err, session) => {
+        if (err) {
+          return res.send({
+            success: false,
+            message: "Server Error",
+          });
+        }
+  
+        res.send({
+          success: true,
+          message: "User Successfully Signed In",
+          token: session.id,
+        });
+      });
+    }
+  );
 });
 
 module.exports = router;
